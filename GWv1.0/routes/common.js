2419be0b94538653dd70ccc5a01fec2e49d07afc
ymanjuy <manjuy124@gmail.com>
2017-07-07 21:46:23 +0530

    save

--- GWv1.0/routes/common.js
+++ GWv1.0/routes/common.js
@@ -13,7 +13,7 @@ var Common = {
 	
 	Schema:require('mongoose').Schema,
 	
-	conn:require('mongoose').createConnection('mongodb://127.0.0.1/GWv1_DB'),
+	conn:require('mongoose').createConnection('mongodb:admin:admin@ds151702.mlab.com:51702/guestworld'),//mongodb://127.0.0.1/GWv1_DB
 	
 	// check if the user is authenticated and can access resources 
 	ensureAuthenticated : function ensureAuthenticated(req, res, next) {
